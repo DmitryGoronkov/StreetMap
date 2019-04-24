3f2d5527157b5e4f2ba7b6fe5bc8af526c0c5be0
Dmitry Goronkov <48731118+DmitryGoronkov@users.noreply.github.com>
2019-04-24 07:52:39 -0400

    Create Greetings.js

---
+++ Greetings.js
@@ -0,0 +1,8 @@
+var greetings;
+greetings = "Hi from Dima!";
+var Alexey;
+if (Alexey is happy to hear from Dima) {
+  console.log ("New message to Dima from ALexey");
+ else
+  console.log(".....");
+ 
